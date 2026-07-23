3525702e1f0f1f176c15c81cc91077ab87f17d10
mmaidan <71332863+mmaidan@users.noreply.github.com>
2026-07-23 15:32:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDtqhO3Q3OsaUr6vGVWZ_Orr3jqW7TGPGI",
-  authDomain: "cobro-isj.firebaseapp.com",
-  projectId: "cobro-isj",
-  storageBucket: "cobro-isj.firebasestorage.app",
-  messagingSenderId: "291983311719",
-  appId: "1:291983311719:web:0a73e7a490f4902fc925c2",
-  measurementId: "G-8YS50BLQM8"
+window.CONFIG = {
+  firebaseConfig: {
+    apiKey: "TU-API-KEY",
+    authDomain: "TU-PROYECTO.firebaseapp.com",
+    projectId: "TU-PROYECTO",
+    storageBucket: "TU-PROYECTO.appspot.com",
+    messagingSenderId: "TU-SENDER-ID",
+    appId: "TU-APP-ID"
+  }
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+ 
